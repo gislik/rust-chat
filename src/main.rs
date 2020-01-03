5331aa2b6aaf66df1489209fb45049751ddcf595
Gísli Kristjánsson <gislik@hamstur.is>
2020-01-03 15:49:14 +0000

    Client struct

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::net::{TcpListener, TcpStream};
+use std::net;
 use std::thread;
 
-fn handle_client(stream: TcpStream) -> io::Result<()> {
-    let (peer, local) = (stream.peer_addr()?, stream.local_addr()?);
-    println!("{} -> {}", peer, local);
-    thread::spawn(move || -> io::Result<()> {
-        for line in BufReader::new(stream).lines() {
-            println!("{}", line?);
-        }
-        println!("{} ~> {}", peer, local);
+struct Client {
+    stream: net::TcpStream,
+}
+
+impl Client {
+    fn new(stream: net::TcpStream) -> Self {
+        Client { stream }
+    }
+
+    fn handle(self: Self) -> io::Result<()> {
+        let stream = self.stream;
+        let (peer, local) = (stream.peer_addr()?, stream.local_addr()?);
+        println!("{} -> {}", peer, local);
+        thread::spawn(move || -> io::Result<()> {
+            for line in BufReader::new(stream).lines() {
+                println!("{} {}", peer, line?);
+            }
+            println!("{} ~> {}", peer, local);
+            Ok(())
+        });
         Ok(())
-    });
-    Ok(())
+    }
 }
 
 fn main() -> io::Result<()> {
-    println!("Hello, world!");
-    let listener = TcpListener::bind("localhost:1234")?;
+    println!("chat server");
+    let listener = net::TcpListener::bind("localhost:1234")?;
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_client(stream),
+            Ok(stream) => Client::new(stream).handle(),
             Err(e) => Err(e),
         }?;
     }
